33780d1de9516febdf67807eeb1c55a1331b7b19
AlfredNico <alfrednicotsu@gmail.com>
2019-10-09 11:04:58 +0300

    interation client

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -6,14 +6,21 @@ use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ClientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('adresse')
+            ->add('nom', TextType::class, ['attr' => [
+                'placeholder' => "Enter le Nom",
+                'class' => "form-control" ]
+            ])
+            ->add('adresse', TextType::class, ['attr' => [
+                'placeholder' => "Enter l' Adressee",
+                'class' => "form-control" ]
+            ])
         ;
     }
 
